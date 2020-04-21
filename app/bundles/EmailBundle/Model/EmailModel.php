a99b1990e3002e3ce5597ef9839ef65b3d2d145b
Zdeno Kuzmany <zdeno@kuzmany.biz>
2020-04-21 09:15:34 +0200

    CS fixer

--- app/bundles/EmailBundle/Model/EmailModel.php
+++ app/bundles/EmailBundle/Model/EmailModel.php
@@ -1771,11 +1771,8 @@ class EmailModel extends FormModel implements AjaxLookupModelInterface
     }
 
     /**
-     * @param string    $column
-     * @param \DateTime $dateFrom
-     * @param \DateTime $dateTo
-     * @param array     $filter
-     * @param bool      $canViewOthers
+     * @param string $column
+     * @param bool   $canViewOthers
      *
      * @return array
      */
@@ -1786,7 +1783,7 @@ class EmailModel extends FormModel implements AjaxLookupModelInterface
         $segmentId  = ArrayHelper::pickValue('segmentId', $filter);
 
         $format = '%H:00';
-        if ($timeFormat == 12) {
+        if (12 == $timeFormat) {
             $format = '%h %p';
         }
 
