ee974eb2bd866b52b146d8ed00598ff937fe3414
Zdeno Kuzmany <zdeno@kuzmany.biz>
2020-07-04 15:41:55 +0200

    Hide more options

--- app/config/security_local.php
+++ app/config/security_local.php
@@ -17,6 +17,25 @@ $container->setParameter('mautic.security.disableUpdates', true);
 
 // hide Identify visitor by device fingerprint and Identify visitors by IP options
 $myCutomRestrictedConfigFields = [
+    'site_url',
+    'webroot',
+    'update_stability',
+    'cache_path',
+    'log_path',
+    'image_path',
+    'cached_data_timeout',
+    'trusted_hosts',
+    'trusted_proxies',
+    'cors_restrict_domains',
+    'max_entity_lock_time',
+    'track_by_tracking_url',
+    'anonymize_ip',
+    'track_contact_by_ip',
+    'queue_protocol',
+    'upload_dir',
+    'max_size',
+    'api_enable_basic_auth',
+    'cors_restrict_domains',
     'ip_lookup_service',
     'ip_lookup_auth',
     'ip_lookup_create_organization',
@@ -28,6 +47,17 @@ $myCutomRestrictedConfigFields = [
     'mailer_spool_recover_timeout',
     'mailer_spool_recover_timeout',
     'mailer_spool_clear_timeout',
+    'queue_mode',
+    'events_orderby_dir',
+    'saml_idp_metadata',
+    'saml_idp_default_role',
+    'saml_idp_email_attribute',
+    'saml_idp_username_attribute',
+    'saml_idp_firstname_attribute',
+    'saml_idp_lastname_attribute',
+    'saml_idp_own_certificate',
+    'saml_idp_own_private_key',
+    'saml_idp_own_password',
 ];
 
 $restrictedConfigFields = array_merge($restrictedConfigFields, $myCutomRestrictedConfigFields);
