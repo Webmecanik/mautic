1c38af61de0bd4b99cad72b367ee03d0273f6f80
Zdeno Kuzmany <zdeno@kuzmany.biz>
2020-08-11 16:27:59 +0200

    Remove hide two first users. No need it

--- plugins/AutomationBundle/Assets/js/automation.js
+++ plugins/AutomationBundle/Assets/js/automation.js
@@ -23,12 +23,7 @@ Mautic.userOnLoad = function (container, response) {
 
     Mautic.userOnLoadCore(container, response);
 
-    if (mQuery(container + ' #list-search').length) {
-        mQuery('#toolbar, #userTable tbody tr:first-child, #userTable tbody tr:first-child + tr').hide();
-        mQuery('.user-list tr').each(function () {
-            mQuery(this).children('*:first').remove();
-        })
-    }else{
+    if (mQuery(container + ' #list-search').length < 1) {
         mQuery('#user_firstName, #user_lastName, #user_username, #user_email').attr('readonly', 'readonly');
         mQuery('#user_plainPassword').remove();
         mQuery('#user_plainPassword_password, #user_plainPassword_confirm').parents('.row').remove();
