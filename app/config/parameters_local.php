b32011adaadd441b4127a0245dc7183b269fed8d
Zdeno Kuzmany <zdeno@kuzmany.biz>
2020-10-13 19:52:33 +0200

    Change default email template

--- app/config/parameters_local.php
+++ app/config/parameters_local.php
@@ -2,6 +2,6 @@
 
 if (!empty($parameters['brick_builder_enable'])) {
     $parameters = [
-        'theme_email_default' => 'blank-new',
+        'theme_email_default' => 'blank-as-email',
     ];
 }
