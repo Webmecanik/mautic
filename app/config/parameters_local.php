3b2f4138eff140090bcf841b6a465a06dd8e2c4d
Zdeno Kuzmany <zdeno@kuzmany.biz>
2020-10-15 15:57:18 +0200

    Fix parameters_local to pretend be an array

--- app/config/parameters_local.php
+++ app/config/parameters_local.php
@@ -1,7 +1,9 @@
 <?php
 
+if (!isset($parameters)) {
+    $parameters = [];
+}
+
 if (!empty($parameters['brick_builder_enable'])) {
-    $parameters = [
-        'theme_email_default' => 'blank-as-email',
-    ];
+    $parameters['theme_email_default'] = 'blank-as-email';
 }
